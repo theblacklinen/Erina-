02869b2f0eac61bcf33358bacd37419c05702d06
Tarun Chauhan <90379712+theblacklinen@users.noreply.github.com>
2022-11-01 13:49:59 +0530

    Add files via upload

---
+++ ErinaBot/modules/alive.py
@@ -0,0 +1,16 @@
+"""@Kaizuryu"""
+
+from telethon import events, Button, custom
+from ErinaBot.events import register
+from ErinaBot import telethn as tbot
+PHOTO = "https://te.legra.ph/file/dfd7cd7877f71c10b0570.jpg"
+@register(pattern=("/alive"))
+async def awake(event):
+  STB = event.sender.first_name
+  STB = "**I m Erina** \n\n" + "**I'm Working Properly**\n\n"
+  STB += "**Python Version : 3.9.7**\n\n"
+  STB += "**python-Telegram-Bot : 13.7**\n\n"
+  BUTTON = [[Button.url("Support", "https://t.me/ErinaSupport"), Button.url("Updates", "https://t.me/ErinaUpdate")]]
+  await tbot.send_file(event.chat_id, Erina, caption=STB,  buttons=BUTTON)
+
+  # thanks to stb the gay
